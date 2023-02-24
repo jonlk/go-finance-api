453fe75dc065b84ccb7632dada14e9041c355cd6
jonlk <jlkovach01@gmail.com>
2023-02-24 16:37:12 -0500

    added error handler middleware
    Co-authored-by: vertwave <109901579+vertwave@users.noreply.github.com>

--- web/front.go
+++ web/front.go
@@ -1,10 +1,26 @@
 package web
 
 import (
+	"log"
+
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterMiddleware(router *gin.Engine) {
 	router.Use(gzip.Gzip(gzip.BestSpeed))
+	router.Use(errorHandler)
+}
+
+var errorHandler = func(ctx *gin.Context) {
+
+	ctx.Next()
+
+	for _, err := range ctx.Errors {
+		log.Print(map[string]any{
+			"Err":  err.Error(),
+			"Type": err.Type,
+			"Meta": err.Meta,
+		})
+	}
 }
