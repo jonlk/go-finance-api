1d81569712c80fd93810b12bd3799843c3f79db4
jonlk <jlkovach01@gmail.com>
2023-02-24 02:17:45 -0500

    updated docker for release

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jonlk/go-finance-api/finance"
 )
 
 func main() {
-	//gin.SetMode(gin.ReleaseMode)
+
+	if len(os.Args) > 1 && os.Args[1] == "-r" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	router := gin.Default()
 	apiGroup := router.Group("/api")
 	finance.RegisterRoutes(apiGroup)
