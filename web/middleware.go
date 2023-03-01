ba730fa076da34426f9f4a87a32d609256651dbd
jonlk <jlkovach01@gmail.com>
2023-03-01 01:01:38 -0500

    modified error handling

--- web/middleware.go
+++ web/middleware.go
@@ -2,6 +2,8 @@ package web
 
 import (
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
@@ -9,29 +11,20 @@ import (
 
 func registerMiddleware(router *gin.Engine) {
 	router.Use(gzip.Gzip(gzip.BestSpeed))
-	router.Use(exampleCustomMiddleware)
 	router.Use(errorHandler)
 }
 
-var exampleCustomMiddleware = func(ctx *gin.Context) {
-
-	//Retrieve a header on request
-	interceptedHeader := ctx.GetHeader("Content-Type")
-	log.Printf("The Content-Type header is: %v\n", interceptedHeader)
-
-	//Or add a custom return header in response
-	ctx.Header("Custom-Header", "Returned to client")
-
-	ctx.Next()
-}
-
 var errorHandler = func(ctx *gin.Context) {
-	for _, err := range ctx.Errors {
-		log.Print(map[string]any{
-			"Err":  err.Error(),
-			"Type": err.Type,
-			"Meta": err.Meta,
-		})
-	}
 	ctx.Next()
+
+	if len(ctx.Errors) > 0 {
+		for _, err := range ctx.Errors {
+			log.Print(map[string]any{
+				"Err":  err.Error(),
+				"Type": err.Type,
+				"Meta": err.Meta,
+			})
+		}
+		ctx.String(http.StatusBadRequest, strings.Join(ctx.Errors.Errors(), ","))
+	}
 }
